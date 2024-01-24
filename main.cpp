61f3c425e2ffe67e77f56f5466074f08838afc2c
Smartisek <domalipdominik@gmail.com>
2024-01-24 13:47:03 +0000

    question 5 shapes

--- main.cpp
+++ main.cpp
@@ -8,14 +8,47 @@ void question1a();
 void question2();
 void question3();
 void question4();
+void question5();
 
 int main() {
 //    question1();
 //    question1a();
 //    question2();
 //    question3();
-        question4();
+//    question4();
+      question5();
     return 0;
+}
+
+void question5(){
+    //triangle
+   int size = 5;
+
+   for(int i =1; i <= size; i++){
+       cout << string(size-i, ' ');
+       cout << string (i*2-1, '*') << endl;
+   }
+
+    cout << "\n";
+
+    //box
+    for(int o= 0; o < 5; o++){
+        cout << endl;
+        for(int p=0; p <= 10; p++){
+            cout << "*";
+        }
+    }
+
+    cout << "\n";
+//rectangle
+    for(int o= 0; o <= 5; o++){
+        cout << endl;
+        for(int p=0; p <= 5; p++){
+            cout << "*";
+        }
+    }
+
+    cout << "\n";
 }
 
 void question4(){
